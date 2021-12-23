9e4079d6d5a8babbfd974b4b366a006c76e8b2c9
Alibek <alibek1991@gmail.com>
2021-12-23 21:58:30 +0600

    Assignment 4 updated

--- e3_module_4.js
+++ e3_module_4.js
@@ -3,3 +3,17 @@
 
 // Например, пользователь ввёл числа 5 и 15. 
 // Каждую секунду в консоль должно печататься число, начиная с 5 и заканчивая 15 (всего 11 чисел: 5 6 7 8 9 10 11 12 13 14 15).
+
+function printNumbers(from, to) {
+  let current = from;
+
+  let timerId = setInterval(function() {
+    console.log(current);
+    if (current == to) {
+      clearInterval(timerId);
+    }
+    current++;
+  }, 1000);
+}
+
+printNumbers(5, 10);
